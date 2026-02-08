edd987f7937a31c22b14c49fe0b9e78f6197cb0d
blasted-tiara <enver.podgorcevic@gmail.com>
2026-02-08 01:46:15 +0100

    Add particles

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,54 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use rand::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[derive(Clone, Copy, Debug)]
+struct Vector2 {
+    x: f32,
+    y: f32,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, quad-trees!");
+impl Vector2 {
+    fn move_random(&mut self, strength: f32, width: f32, height: f32) {
+        let mut rng = rand::rng();
+
+        self.x = (self.x + rng.random::<f32>() * strength).clamp(0.0_f32, width);
+        self.y = (self.y + rng.random::<f32>() * strength).clamp(0.0_f32, height);
+    }
+}
+
+#[wasm_bindgen]
+struct Universe {
+    width: f32,
+    height: f32,
+    particles: Vec<Vector2>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new(width: f32, height: f32, particle_count: usize) -> Self {
+        let mut rng = rand::rng();
+        let mut particles = Vec::new();
+        for _ in 0..particle_count {
+            particles.push(Vector2 {
+                x: rng.random::<f32>() * width,
+                y: rng.random::<f32>() * height,
+            });
+        };
+
+        Self {
+            width,
+            height,
+            particles,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        for particle in &mut self.particles {
+            particle.move_random(1.0, self.width, self.height);
+        }
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,9 @@
-import * as wasm from "quad-trees";
+import { Universe } from "quad-trees";
 
-wasm.greet();
+const UNIVERSE_WIDTH = 50.0;
+const UNIVERSE_HEIGHT = 50.0;
+const UNIVERSE_PARTICLE_COUNT = 100;
+
+const pre = document.getElementById("quad-trees-canvas");
+const universe = Universe.new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT, UNIVERSE_PARTICLE_COUNT);
+
