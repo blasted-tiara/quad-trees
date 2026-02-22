411962b23dd6cb378d4683aed7d7e856a6aff720
blasted-tiara <enver.podgorcevic@gmail.com>
2026-02-22 10:23:30 +0100

    it's alive! the particle system is alive!

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::fmt;
+
 use wasm_bindgen::prelude::*;
 use rand::prelude::*;
 
@@ -15,8 +17,8 @@ impl Vector2 {
     fn move_random(&mut self, strength: f32, width: f32, height: f32) {
         let mut rng = rand::rng();
 
-        self.x = (self.x + rng.random::<f32>() * strength).clamp(0.0_f32, width);
-        self.y = (self.y + rng.random::<f32>() * strength).clamp(0.0_f32, height);
+        self.x = (self.x + (rng.random::<f32>() - 0.5) * strength).clamp(0.0_f32, width);
+        self.y = (self.y + (rng.random::<f32>() - 0.5) * strength).clamp(0.0_f32, height);
     }
 }
 
@@ -50,5 +52,38 @@ impl Universe {
         for particle in &mut self.particles {
             particle.move_random(1.0, self.width, self.height);
         }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for j in 0..20 {
+            for i in 0..20 {
+                // check if cell is occupied
+                let left = i as f32 * self.width / 20.0;
+                let right = (i + 1) as f32 * self.width / 20.0;
+                let up = j as f32 * self.height / 20.0;
+                let down = (j + 1) as f32 * self.height / 20.0;
+
+                let mut symbol =  '◻';
+                for particle in self.particles.as_slice() {
+                    if 
+                        particle.x < right && 
+                        particle.x > left &&
+                        particle.y < down &&
+                        particle.y > up {
+                            symbol = '◼';
+                            break;
+                    }
+                }
+                write!(f, "{}", symbol);
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,9 +1,17 @@
 import { Universe } from "quad-trees";
 
-const UNIVERSE_WIDTH = 50.0;
-const UNIVERSE_HEIGHT = 50.0;
+const UNIVERSE_WIDTH = 80.0;
+const UNIVERSE_HEIGHT = 80.0;
 const UNIVERSE_PARTICLE_COUNT = 100;
 
 const pre = document.getElementById("quad-trees-canvas");
 const universe = Universe.new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT, UNIVERSE_PARTICLE_COUNT);
 
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+}
+
+requestAnimationFrame(renderLoop);
