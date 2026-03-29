7d9ada35fe402c7e181bd9c227a59ede9c86264d
blasted-tiara <enver.podgorcevic@gmail.com>
2026-03-29 17:51:38 +0200

    behold, a webgpu square

--- www/index.js
+++ www/index.js
@@ -8,8 +8,8 @@ const UNIVERSE_PARTICLE_COUNT = 600;
 const PARTICLE_RADIUS = 3.0;
 const PARTICLE_COLOR = '#999999';
 
-const CANVAS_WIDTH = 1000;
-const CANVAS_HEIGHT = 1000;
+const CANVAS_WIDTH = 1024;
+const CANVAS_HEIGHT = 1024;
 
 const universe = Universe.new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT, UNIVERSE_PARTICLE_COUNT);
 const particle_count = universe.particle_count();
@@ -17,17 +17,115 @@ const particle_count = universe.particle_count();
 const canvas = document.getElementById("quad-trees-canvas");
 canvas.height = CANVAS_HEIGHT;
 canvas.width = CANVAS_WIDTH;
-const ctx = canvas.getContext('2d');
+
+// Web gpu init
+if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.")
+}
+
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+    throw new Error("No appropriate GPUAdapter found.")
+}
+
+const device = await adapter.requestDevice();
+
+const ctx = canvas.getContext('webgpu');
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+ctx.configure({
+    device: device,
+    format:canvasFormat,
+});
+
+const vertices = new Float32Array([
+    -0.8, -0.8,
+    0.8, -0.8,
+    0.8,  0.8,
+
+    -0.8, -0.8,
+    0.8,  0.8,
+    -0.8,  0.8,
+]);
+
+const vertexBuffer = device.createBuffer({
+    label: "Cell vertices",
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+const vertexBufferLayout = {
+    arrayStride: 8,
+    attributes: [{
+        format: "float32x2",
+        offset: 0,
+        shaderLocation: 0, // Position, see vertex shader
+    }],
+};
+
+const cellShaderModule = device.createShaderModule({
+    label: "Cell shader",
+    code: `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+        return vec4f(pos, 0, 1);
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+        return vec4f(1, 0, 0, 1);
+    }
+  `
+});
+
+const cellPipeline = device.createRenderPipeline({
+    label: "Cell pipeline",
+    layout: "auto",
+    vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout]
+    },
+    fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+            format: canvasFormat
+        }]
+    }
+});
+
+const encoder = device.createCommandEncoder();
 
 const renderLoop = () => {
-    drawParticles();
-    universe.tick();
+    const pass = encoder.beginRenderPass({
+        colorAttachments: [{
+            view: ctx.getCurrentTexture().createView(),
+            loadOp: "clear",
+            clearValue: { r: 0, g: 0, b: 0.4, a: 1 },
+            storeOp: "store",
+        }]
+    });
 
-    requestAnimationFrame(renderLoop);
+    pass.setPipeline(cellPipeline);
+    pass.setVertexBuffer(0, vertexBuffer);
+    pass.draw(vertices.length / 2); // 6 vertices
+
+    //draw();
+    //universe.tick();
+
+    pass.end();
+    device.queue.submit([encoder.finish()]);
+
+    // requestAnimationFrame(renderLoop);
+}
+
+const draw = () => {
+    drawParticles();
 }
 
 const drawParticles = () => {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     const particlesPtr = universe.particle_ptr();
     const particles = new Float32Array(memory.buffer, particlesPtr, particle_count);
 
